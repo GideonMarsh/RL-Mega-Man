a53da191d150dac46bed40ff39d7f141e34e0b52
GideonMarsh <gidmarsh@gmail.com>
2021-01-26 19:04:30 -0800

    Started work on capturing screen

---
+++ driver.py
@@ -0,0 +1,29 @@
+# Gideon Marsh
+# github.com/GideonMarsh
+
+
+from screenshotter import takescreenshot
+from screenshotter import findBounds
+import math
+import time
+#screenshot = takescreenshot('Mega Man Legacy Collection')
+
+#hwnd = win32gui.FindWindow(None, 'Mega Man Legacy Collection')
+#win32gui.SetForegroundWindow(hwnd)
+
+time.sleep(3)
+count = 0
+#while True:
+region = findBounds('Mega Man Legacy Collection')
+screenshot = takescreenshot('Mega Man Legacy Collection', region)
+if (screenshot):
+    grayimg = screenshot.convert('L')
+    pix = grayimg.load()
+    val = 0
+    for i in range(math.floor(grayimg.width)):
+        for j in range(math.floor(grayimg.height)):
+            val = val + pix[i,j]
+    count = count + 1
+    print(count)
+
+    grayimg.save('testscreenshot.png')
---
+++ screenshotter.py
@@ -0,0 +1,63 @@
+# Gideon Marsh
+# github.com/GideonMarsh
+
+import pyautogui
+import win32gui
+import time
+import math
+
+def takescreenshot(windowName=None, r=None):
+    if r:
+        hwnd = win32gui.FindWindow(None, windowName)
+        if hwnd:
+            win32gui.SetForegroundWindow(hwnd)
+            x, y, x1, y1 = win32gui.GetClientRect(hwnd)
+            x, y = win32gui.ClientToScreen(hwnd, (x, y))
+            print(x)
+            im = pyautogui.screenshot(region=(r[0] + x,r[1] + y,r[2],r[3]))
+            return im
+        else:
+            print('Window not found!')
+    else:
+        im = pyautogui.screenshot()
+        return im
+
+def findBounds(windowName=None):
+    if windowName:
+        hwnd = win32gui.FindWindow(None, windowName)
+        if hwnd:
+            win32gui.SetForegroundWindow(hwnd)
+            time.sleep(0.1)
+            x, y, x1, y1 = win32gui.GetClientRect(hwnd)
+            x, y = win32gui.ClientToScreen(hwnd, (x, y))
+            x1, y1 = win32gui.ClientToScreen(hwnd, (x1 - x, y1 - y))
+            im = pyautogui.screenshot(region=(x, y, x1, y1))
+
+            pix = im.load()
+            bx = by = bw = bh = 0
+            previous = (0,0,0)
+            for i in range(x1):
+                if (previous != pix[i,math.floor(y1 / 2)]):
+                    bx = i
+                    break
+            previous = (0,0,0)
+            for i in range(y1):
+                if (previous != pix[math.floor(x1 / 2),i]):
+                    by = i
+                    break
+            previous = (0,0,0)
+            for i in range(x1):
+                if (previous != pix[x1 - i - 1,math.floor(y1 / 2)]):
+                    bw = (x1 - i) - bx
+                    break
+            previous = (0,0,0)
+            for i in range(y1):
+                if (previous != pix[math.floor(x1 / 2),y1 - i - 1]):
+                    bh = (y1 - i) - by
+                    break
+
+            return (bx, by, bw, bh)
+        else:
+            print("Window not found!")
+    else:
+        print("Window needs a name!")
