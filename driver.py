67e1df2cf3d214cfa631dfa71a2add84d19ed3d3
GideonMarsh <gidmarsh@gmail.com>
2021-01-28 12:04:34 -0800

    Tested time it takes to take screenshot

--- driver.py
+++ driver.py
@@ -14,26 +14,28 @@ yPixels = 200
 
 screenshotter.setWindowSize(windowName)
 time.sleep(3)
-#while True:
 region = screenshotter.findBounds(windowName)
 if (region[3] < yPixels or region[2] < xPixels):
     print('Screen not large enough')
-screenshot = screenshotter.takescreenshot(windowName, region)
-if (screenshot):
-    grayimg = screenshot.convert('L')
-    pix = grayimg.load()
-    yOffset = math.floor(grayimg.height / yPixels)
-    xOffset = math.floor(grayimg.width / xPixels)
-    xShift = math.floor((grayimg.width % xPixels) / 2)
-    yShift = math.floor((grayimg.height % yPixels) / 2)
-    for i in range(xPixels):
-        for j in range(yPixels):
-            pix[i,j] = pix[(i * xOffset) + xShift, (j * yOffset) + yShift]
+while True:
+    previousTime = time.time()
+    screenshot = screenshotter.takescreenshot(windowName, region)
+    if (screenshot):
+        grayimg = screenshot.convert('L')
+        pix = grayimg.load()
 
-    #for i in range(math.floor(grayimg.width)):
-    #    for j in range(math.floor(grayimg.height)):
-    print(grayimg.width)
-    print(grayimg.height)
+        yOffset = math.floor(grayimg.height / yPixels)
+        xOffset = math.floor(grayimg.width / xPixels)
+        xShift = math.floor((grayimg.width % xPixels) / 2)
+        yShift = math.floor((grayimg.height % yPixels) / 2)
 
+        for i in range(xPixels):
+            for j in range(yPixels):
+                pix[i,j] = pix[(i * xOffset) + xShift, (j * yOffset) + yShift]
 
-    grayimg.save('testscreenshot.png')
+        print(time.time() - previousTime)
+
+    #print(grayimg.width)
+    #print(grayimg.height)
+
+    #grayimg.save('testscreenshot.png')
