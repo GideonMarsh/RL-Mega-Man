59794883285ed3801a317f796ed499348de38d95
GideonMarsh <gidmarsh@gmail.com>
2021-02-02 16:19:24 -0800

    Removed unnecessary NodeGene class, replaced with dictionary

--- brain.py
+++ brain.py
@@ -16,16 +16,6 @@ Nodes with any other innovation number are hidden nodes
 nodeCount = INPUT_NODES + OUTPUT_NODES
 connectionCount = 0
 
-class NodeGene:
-    def __init__(self, innovationNumber=None):
-        if (innovationNumber):
-            self.inum = innovationNumber
-        else:
-            global nodeCount
-            nodeCount = nodeCount + 1
-            self.inum = nodeCount
-        self.value = 0
-
 class ConnectionGene:
     def __init__(self, inNode, outNode, weight=0, innovationNumber=None):
         if (innovationNumber):
@@ -48,7 +38,10 @@ class ConnectionGene:
 
     def calculateValue(self, nodes, futureNodes):
         if (self.enabled):
-            nodes[self.outNode].value = nodes[self.outNode].value + nodes[self.inNode].value * self.weight
+            if (self.outNode in nodes):
+                nodes[self.outNode] = nodes[self.outNode] + nodes[self.inNode] * self.weight
+            else:
+                nodes[self.outNode] = nodes[self.inNode] * self.weight
         futureNodes.append(self.outNode)
         if (self.nextConnection):
             self.nextConnection.calculateValue(nodes, futureNodes)
@@ -56,16 +49,12 @@ class ConnectionGene:
 class Brain:
     def __init__(self):
         self.fitness = 0
-        self.nodes = {}
         self.connections = {}
 
     # make this brain from scratch, with only input and output nodes and no connections
     # should only be used when creating the initial population
     def initNewBrain(self):
-        for i in range(INPUT_NODES):
-            self.nodes[i + 1] = NodeGene(i + 1)
-        for i in range(OUTPUT_NODES):
-            self.nodes[i + 1 + INPUT_NODES] = NodeGene(i + 1 + INPUT_NODES)
+        pass
 
     # make this brain the offspring of two parents
     def crossover(self, parentA, parentB):
@@ -138,30 +127,29 @@ class Brain:
         d = (c1 * E) / N + (c2 * D) / N + c3 * W
         return d
 
-
-
-
     # calculate the outputs based on given inputs
     def think(self, inputs):
-        # reset all node values
-        for n in self.nodes.values():
-            n.value = 0
+
+        nodes = {}
         # set inputs
         nodesToCalculate = list()
         for i in range(INPUT_NODES):
-            self.nodes[i + 1].value = inputs[i]
+            nodes[i + 1] = inputs[i]
             nodesToCalculate.append(i + 1)
 
         # calculate values
         while (len(nodesToCalculate) > 0):
             nextNode = nodesToCalculate.pop(0)
             if (nextNode in self.connections):
-                self.connections[nextNode].calculateValue(self.nodes, nodesToCalculate)
+                self.connections[nextNode].calculateValue(nodes, nodesToCalculate)
 
         # return outputs
         outputs = list()
         for i in range(OUTPUT_NODES):
-            outputs.append(self.nodes[i + 1 + INPUT_NODES].value)
+            if (i + 1 + INPUT_NODES in nodes):
+                outputs.append(nodes[i + 1 + INPUT_NODES])
+            else:
+                outputs.append(0)
         return outputs
 
     # check if a node is later in the tree than the given node
@@ -184,16 +172,16 @@ class Brain:
     # takes the inum values of the nodes as input, not the node objects
     def addNewConnection(self, inNode, outNode, weight=0, innovationNumber=None):
         # return false if the end of the connection is an input node
-        if (self.nodes[outNode].inum <= INPUT_NODES):
+        if (outNode <= INPUT_NODES):
             raise ValueError('Connections cannot end at an input node!')
         # return false if the beginning of the connection is an output node
-        if (self.nodes[inNode].inum > INPUT_NODES and self.nodes[inNode].inum <= INPUT_NODES + OUTPUT_NODES):
+        if (inNode > INPUT_NODES and inNode <= INPUT_NODES + OUTPUT_NODES):
             raise ValueError('Connections cannot start at an output node!')
         # return false if this connection will create a cycle
         if (self.isNodeLaterOnPath(outNode, inNode)):
             raise ValueError('Connections cannot create cycles!')
 
-        # create the connection and return true
+        # create the connection
         newConnection = ConnectionGene(inNode, outNode, weight, innovationNumber)
         if (inNode in self.connections):
             self.connections[inNode].addNewConnection(newConnection)
@@ -202,15 +190,15 @@ class Brain:
 
     # adds a new node in the middle of an existing connection, modifying connections as necessary
     def addNewNode(self, oldConnection):
-        newNode = NodeGene()
-        self.nodes[newNode.inum] = newNode
+        global nodeCount
+        nodeCount = nodeCount + 1
 
         oldConnection.enabled = False
 
-        self.addNewConnection(oldConnection.inNode, newNode.inum, oldConnection.weight)
-        self.addNewConnection(newNode.inum, oldConnection.outNode, oldConnection.weight)
+        self.addNewConnection(oldConnection.inNode, nodeCount, oldConnection.weight)
+        self.addNewConnection(nodeCount, oldConnection.outNode, oldConnection.weight)
 
-    # return a list of all connections
+    # returns a list of all connections
     def getAllConnections(self):
         allConnections = list()
         baseConnections = list(self.connections.values())
@@ -224,6 +212,19 @@ class Brain:
                 allConnections.append(c)
 
         return allConnections
+
+    # returns a list of all nodes, which is inferred from all connections
+    def getAllNodes(self):
+        allConnections = self.getAllConnections()
+        nodes = list()
+        for i in range(INPUT_NODES + OUTPUT_NODES):
+            nodes.append(i+1)
+        for c in allConnections:
+            if c.inNode not in nodes:
+                nodes.append(c.inNode)
+            if c.outNode not in nodes:
+                nodes.append(c.outNode)
+        return nodes
 
     # make one random structural mutation to the neural network
     # if there are no connections, add a connection
@@ -249,12 +250,12 @@ class Brain:
             Once a valid connection has been found, it will be made and the function will return
             If there are no valid connections remaining in the structure, function will return
             '''
-            startNodes = list(self.nodes.values())
+            startNodes = self.getAllNodes()
             while (len(startNodes) > 0):
                 # step 1
                 startIndex = floor(len(startNodes) * random())
                 s = startNodes.pop(startIndex)
-                endNodes = list(self.nodes.values())
+                endNodes = self.getAllNodes()
                 while (len(endNodes) > 0):
                     # step 2
                     endIndex = floor(len(endNodes) * random())
@@ -262,7 +263,7 @@ class Brain:
                     try:
                         # step 3
                         for c in allConnections:
-                            if (c.inNode == s.inum and c.outNode == e.inum):
+                            if (c.inNode == s and c.outNode == e):
                                 if (c.enabled):
                                     raise ValueError('Connection already exists!')
                                 else:
@@ -270,7 +271,7 @@ class Brain:
                                     c.enabled = True
                                     return
                         # step 4
-                        self.addNewConnection(s.inum, e.inum, w)
+                        self.addNewConnection(s, e, w)
                         # step 4a
                         return
                     except ValueError:
@@ -297,25 +298,25 @@ a = Brain()
 b = Brain()
 a.initNewBrain()
 b.initNewBrain()
-'''
-print(len(list(a.nodes.values())))
+
+print(len(a.getAllNodes()))
 print(len(a.getAllConnections()))
 print(a.think((1,2,2)))
-'''
+
 a.mutateStructure()
-'''
-print(len(list(a.nodes.values())))
+
+print(len(a.getAllNodes()))
 print(len(a.getAllConnections()))
 print(a.think((1,2,2)))
-'''
+
 b.mutateStructure()
 a.mutateStructure()
 b.mutateStructure()
 a.mutateStructure()
 b.mutateStructure()
-print(len(list(a.nodes.values())))
+print(len(a.getAllNodes()))
 print(len(a.getAllConnections()))
-print(len(list(b.nodes.values())))
+print(len(b.getAllNodes()))
 print(len(b.getAllConnections()))
 print('comparison')
 print(a.compare(b))
